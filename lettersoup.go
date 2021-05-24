910266ddbfeddd8f0350e6cbb9a32ccb1950384d
Juan Pablo Almonacid <jpalmonacidv@gmail.com>
2021-05-24 19:59:48 -0300

    Initial commit

---
+++ lettersoup.go
@@ -0,0 +1,63 @@
+package lettersoup
+
+import (
+	"errors"
+	"fmt"
+)
+
+func countWordMatchesInSoup(word string, soup []string) (count int, err error) {
+	wordLength, rowsCount, colsCount := len(word), len(soup), len(soup[0])
+	if wordLength == 0 {
+		return -1, errors.New("Empty word given.")
+	}
+	if rowsCount == 0 && colsCount == 0 {
+		message := fmt.Sprintf(
+			"Matrix size {W: %d, H: %d} is smaller than word (%d).",
+			rowsCount,
+			colsCount,
+			wordLength,
+		)
+		return -1, errors.New(message)
+	}
+	count = 0
+	for i := 0; i < rowsCount; i += 1 {
+		for j := 0; j < colsCount; j += 1 {
+			if soup[i][j] == word[0] {
+				// Horizontal Left-Right
+				if colsCount-j >= wordLength && soup[i][j:j+wordLength] == word {
+					count += 1
+				}
+				// Horizontal Right-Left
+				if j-(wordLength-1) >= 0 && string(soup[i][j])+string(soup[i][j-1])+string(soup[i][j-2]) == word {
+					count += 1
+				}
+				// Vertical Top-Bottom
+				if rowsCount-i >= wordLength && string(soup[i][j])+string(soup[i+1][j])+string(soup[i+2][j]) == word {
+					count += 1
+				}
+				// Vertical Bottom-Top
+				if i-(wordLength-1) >= 0 && string(soup[i][j])+string(soup[i-1][j])+string(soup[i-2][j]) == word {
+					count += 1
+				}
+				// Diagonal LeftRight-TopBottom
+				if j+(wordLength-1) < colsCount && i+(wordLength-1) < rowsCount && string(soup[i][j])+string(soup[i+1][j+1])+string(soup[i+2][j+2]) == word {
+					count += 1
+				}
+				// Diagonal RightLeft-TopBottom
+				if j-(wordLength-1) >= 0 && i+(wordLength-1) < rowsCount && string(soup[i][j])+string(soup[i+1][j-1])+string(soup[i+2][j-2]) == word {
+					count += 1
+				}
+				// Diagonal LeftRight-BottomTop
+				if j+(wordLength-1) < colsCount && i-(wordLength-1) >= 0 && string(soup[i][j])+string(soup[i-1][j+1])+string(soup[i-2][j+2]) == word {
+					count += 1
+				}
+				// Diagonal RightLeft-BottomTop
+				if j-(wordLength-1) >= 0 && i-(wordLength-1) >= 0 && string(soup[i][j])+string(soup[i-1][j-1])+string(soup[i-2][j-2]) == word {
+					count += 1
+				}
+			}
+		}
+	}
+
+	return count, nil
+}
---
+++ lettersoup_test.go
@@ -0,0 +1,45 @@
+package lettersoup
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func TestGivenInvalidSizeSoupReturnsMinus1AndError(t *testing.T) {
+// 	want := -1
+// 	count, err := CountOIEMatchesInSoup([]string{"OI", "XX", "OY"})
+// 	if count != want || err == nil {
+// 		t.Fatalf(`CountOIEMatchesInSoup("", {"OIE", "XXX", "OYE"}) = ( %d, "%v" ), want ( %d, error )`,
+// 			count,
+// 			err,
+// 			want,
+// 		)
+// 	}
+// }
+
+func TestGivenValidSoupItReturnsCount(t *testing.T) {
+	testCases := []struct {
+		soup        []string
+		wantedCount int
+	}{
+		{[]string{"OIE", "IIX", "EXE"}, 3},
+		{[]string{"EIOIEIOEIO"}, 4},
+		{[]string{"EAEAE", "AIIIA", "EIOIE", "AIIIA", "EAEAE"}, 8},
+		{[]string{"OX", "IO", "EX", "II", "OX", "IE", "EX"}, 3},
+		{[]string{"E"}, 0},
+	}
+	for _, testCase := range testCases {
+		testDescription := fmt.Sprintf("Test case %v", testCase)
+		t.Run(testDescription, func(t *testing.T) {
+			gottenCount, err := countWordMatchesInSoup("OIE", testCase.soup)
+			if gottenCount != testCase.wantedCount || err != nil {
+				t.Fatalf(`countWordMatchesInSoup() = ( %d, "%v" ), want ( %d, nil )`,
+					gottenCount,
+					err,
+					testCase.wantedCount,
+				)
+			}
+		})
+
+	}
+}
